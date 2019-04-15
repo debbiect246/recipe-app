5270b316c75fce3ded2401b6e80bc092772d72b0
Deborah Thompson <deb246@btinternet.com>
2019-04-15 07:49:41 +0000

    app.py updated for login and register routes

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ app.config["MONGO_URI"] = dbconfig()
 
 mongo = PyMongo(app)
 
-
+"""
 #route for home page
 @app.route("/")
 def index():
     """Main page"""
     return render_template("index.html")
-    
+
 
 #route for eventual log in page
     
@@ -33,6 +33,47 @@ def login():
 def user(username):
     """Display welcome message"""
     return "<h1>Welcome {0}</h1>".format(username)
+"""
+
+Home page with login form
+
+@app.route('/')
+def index():
+    return render_template('login.html', register=mongo.db.register.find_one())
+
+"""Register form action  method must be post.  Insert  new user in database using form on login page, then redirect user to allrecipeslist page.
+"""
+
+@app.route('/register',methods=['POST','GET'])
+def register():
+    if request.method == 'POST':
+        register = mongo.db.register
+        reg_id = register.insert_one(request.form.to_dict())
+        # print(register)
+        object_id = reg_id.inserted_id
+        return redirect(url_for('allrecipeslist',register_id=object_id))
+    return render_template('register.html')
+
+"""
+Login page action. Method must be post - find the given password and username and  if it matches then
+redirect to allrecipeslist but ifnot redirect to register and if password only incorrect flash message to show that  password is incorrect
+"""
+@app.route('/login', methods=["GET","POST"])
+def login():
+    if request.method == 'POST':
+        login_user = mongo.db.register.find_one({'username': request.form['username']})
+        form = request.form
+        if login_user:
+            if(form["password"] == login_user["password"]): # if password correct
+                session['username'] = login_user["username"]
+                return redirect(url_for('allrecipeslist',register_id = login_user["_id"]))
+            else: # and if password is not correct
+               flash("Incorrect password") 
+        else:# if not exist
+            flash("User does not exist")
+            return redirect(url_for('register'))
+    return render_template('login.html')
+            
  
 #route to display all recipes in summary and expanded form with accordion dropdown
 
