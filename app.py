9d10372c2b82f77767719fab477f0b9159728db7
Deborah Thompson <deb246@btinternet.com>
2019-05-17 09:39:04 +0000

    debug changed to False in app.py

--- app.py
+++ app.py
@@ -190,4 +190,4 @@ def log_out():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
