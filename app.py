7fde227b7781ae3a099340ac779da669f9924f8e
Deborah Thompson <deb246@btinternet.com>
2019-05-17 18:21:59 +0000

    changed env variables

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ if app.config['DEBUG'] == True:
     app.config["MONGO_URI"] = dbconfig()
 else:
     app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+    app.config['MONGO_DBNAME'] = os.getenv('MONGO_DBNAME')
 
 mongo = PyMongo(app)
 
